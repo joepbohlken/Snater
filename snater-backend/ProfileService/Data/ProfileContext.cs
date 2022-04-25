889fc38885e31a0f06886c970939dd67112bb17e
Bohlken,Joep J <joepbohlken@gmail.com>
2022-04-25 10:28:23 +0200

    wip

--- snater-backend/ProfileService/Data/ProfileContext.cs
+++ snater-backend/ProfileService/Data/ProfileContext.cs
@@ -10,49 +10,6 @@ namespace Snater.Services.Profile.Data
 
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            // Seed Users
-            Guid userId0 = Guid.NewGuid();
-            User user0 = new User()
-            {
-                Id = userId0,
-                Username = "Henk",
-                Status = Enums.UserStatus.DontDisturb
-            };
-            
-            modelBuilder.Entity<User>().HasData(
-                user0);
-
-            Guid userId1 = Guid.NewGuid();
-            modelBuilder.Entity<User>().HasData(
-                new User { Id = userId1, Username = "Pieter", Status = Enums.UserStatus.DontDisturb });
-
-            Guid userId2 = Guid.NewGuid();
-            modelBuilder.Entity<User>().HasData(
-                new User { Id = userId2, Username = "Klaas", Status = Enums.UserStatus.DontDisturb });
-
-            Guid userId3 = Guid.NewGuid();
-            modelBuilder.Entity<User>().HasData(
-                new User { Id = userId3, Username = "Vaas", Status = Enums.UserStatus.DontDisturb });
-
-            Guid userId4 = Guid.NewGuid();
-            modelBuilder.Entity<User>().HasData(
-                new User { Id = userId4, Username = "Peter", Status = Enums.UserStatus.DontDisturb });
-
-            Guid userId5 = Guid.NewGuid();
-            modelBuilder.Entity<User>().HasData(
-                new User { Id = userId5, Username = "Pannenkoek", Status = Enums.UserStatus.DontDisturb });
-
-            Guid userId6 = Guid.NewGuid();
-            modelBuilder.Entity<User>().HasData(
-                new User { Id = userId6, Username = "Daniel", Status = Enums.UserStatus.DontDisturb });
-
-            // Seed Chats
-            Guid chatId0 = Guid.NewGuid();
-            modelBuilder.Entity<User>().HasData(
-                new Chat { Id = chatId0, User = , LastMessage });
-        }
         public DbSet<Chat> Chats { get; set; }
     }
 
---
+++ snater-backend/Snater.Services.Chat/Controllers/MessageController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Snater.Services.Chat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

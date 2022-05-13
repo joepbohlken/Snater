83bcdc54ccd1ecab5ebedf9a5f23cc8950d3cdc1
Bohlken,Joep J <joepbohlken@gmail.com>
2022-05-13 15:11:59 +0200

    trying to get actions to work test 2

--- snater-backend/Snater.Services.Chat/Models/Chat.cs
+++ snater-backend/Snater.Services.Chat/Models/Chat.cs
@@ -4,7 +4,7 @@ namespace Snater.Services.Chat.Models
 {
     public class Chat
     {
-        // test
+        // test 2
         [Key]
         public Guid Id { get; set; }
         //public User User { get; set; }
